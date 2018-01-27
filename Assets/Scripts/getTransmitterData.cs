bd191f8582cac4e512fdf78adcbfd361cca5b921
Dmart331 <dmart331@gmail.com>
2018-01-27 17:13:26 -0600

    Slider and icons

--- Assets/Scripts/getTransmitterData.cs
+++ Assets/Scripts/getTransmitterData.cs
@@ -6,23 +6,30 @@ public class getTransmitterData : MonoBehaviour {
 
 	public MicAnalyzer ma;
 	public RangeDetector rd;
+
 	public GameObject Laser;
 	public GameObject Sat;
 	public GameObject Radio;
 	public GameObject Wifi;
 	public GameObject Bluetooth;
 
-	private bool _canLerp = false;
+
 	public bool _pitchReached = false;
 	private float _riAlpha;
 	private string _transmitterName;
-
+    private List<GameObject> _list_o_jects;
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	void Update()
+	void Start ()
+    {
+        _list_o_jects = new List<GameObject>();
+        _list_o_jects.Add(Laser);
+        _list_o_jects.Add(Sat);
+        _list_o_jects.Add(Radio);
+        _list_o_jects.Add(Wifi);
+        _list_o_jects.Add(Bluetooth);
+    }
+
+    void Update()
 	{
 
 		_transmitterName = rd.getTransmitterFromPitch(ma.curPitch).name;
@@ -30,29 +37,40 @@ public class getTransmitterData : MonoBehaviour {
 			Debug.Log ("name " +_transmitterName);
 		}
 
+        foreach(GameObject go in _list_o_jects)
+        {
+            go.GetComponent<AlphaLerp>().canLerp = false;
+        }
 
-		if (_transmitterName == "Laser") {
-			Laser.GetComponent<AlphaLerp>().canLerp = true;
-			Debug.Log("current pitch" + ma.curPitch);
-		}
-		else if (_transmitterName == "WiFi") {
-			Sat.GetComponent<AlphaLerp>().canLerp = true;
-			Debug.Log("current pitch" + ma.curPitch);
-		}
-		else if (_transmitterName == "Radio") {
-			Radio.GetComponent<AlphaLerp>().canLerp = true;
-			Debug.Log("current pitch" + ma.curPitch);
-		}
-		else if (_transmitterName == "Satellite") {
-			Wifi.GetComponent<AlphaLerp>().canLerp = true;
-			Debug.Log("current pitch" + ma.curPitch);
-		}
-		else if (_transmitterName == "Bluetooth") {
-			Bluetooth.GetComponent<AlphaLerp>().canLerp = true;
-			Debug.Log("current pitch" + ma.curPitch);
-		}
-
-
-
+        CheckTransmitterName(_transmitterName);
 	}
+
+    void CheckTransmitterName(string tn)
+    {
+        if (tn == "Laser")
+        {
+            Laser.GetComponent<AlphaLerp>().canLerp = true;
+            Debug.Log("current pitch" + ma.curPitch);
+        }
+        else if (tn == "WiFi")
+        {
+            Sat.GetComponent<AlphaLerp>().canLerp = true;
+            Debug.Log("current pitch" + ma.curPitch);
+        }
+        else if (tn == "Radio")
+        {
+            Radio.GetComponent<AlphaLerp>().canLerp = true;
+            Debug.Log("current pitch" + ma.curPitch);
+        }
+        else if (tn == "Satellite")
+        {
+            Wifi.GetComponent<AlphaLerp>().canLerp = true;
+            Debug.Log("current pitch" + ma.curPitch);
+        }
+        else if (tn == "Bluetooth")
+        {
+            Bluetooth.GetComponent<AlphaLerp>().canLerp = true;
+            Debug.Log("current pitch" + ma.curPitch);
+        }
+    }
 }
