38c99eaf2657a56b3799daccfa6b5f4aefc9fc61
Rico <richardthomaswhitfield@gmail.com>
2018-01-27 15:40:06 -0600

    Fixed lerp on freq

---
+++ Assets/Scripts/getTransmitterData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getTransmitterData : MonoBehaviour {
+
+	public MicAnalyzer ma;
+	public RangeDetector rd;
+	public GameObject Laser;
+
+	private bool _canLerp = false;
+	public bool _pitchReached = false;
+	private float _riAlpha;
+	private string _transmitterName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update()
+	{
+
+		_transmitterName = rd.getTransmitterFromPitch(ma.curPitch).name;
+		Debug.Log ("name " +_transmitterName);
+
+		if (_transmitterName == "Laser") {
+			Laser.GetComponent<AlphaLerp>().canLerp = true;
+			Debug.Log("current pitch" + ma.curPitch);
+		}
+
+
+
+	}
+}
