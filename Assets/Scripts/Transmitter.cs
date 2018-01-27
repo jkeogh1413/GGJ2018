48879fa242f7e5e87b2c86592d8c07f9b506acfd
JPage4 <jesse.page@me.com>
2018-01-27 16:00:06 -0600

    transmitter position script

---
+++ Assets/Scripts/Transmitter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transmitter : MonoBehaviour
+{
+	public Vector3 transPosition;
+	public string transType;
+	public bool transmissionReceiving;
+
+	void Start()
+	{
+		transPosition = transform.position;
+	}
+
+	public Vector3 getTransPosition()
+	{
+		return transPosition;
+	}
+}
