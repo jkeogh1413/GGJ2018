bc228d972a61e56f319a742bfa2464b68fad5359
Tanner Netterville <tannern@gmail.com>
2018-01-28 15:36:11 -0600

    The way the code was when we recorded videos

---
+++ Assets/GlobalSong.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSong : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().PlayDelayed (15f);	
+	}
+}
--- Assets/Scripts/Transmitter.cs
+++ Assets/Scripts/Transmitter.cs
@@ -4,79 +4,92 @@ using UnityEngine;
 
 public class Transmitter : MonoBehaviour
 {
-	public Vector3 transPosition;
-	public string transType = "Default";
-	public bool withinView = false;
-	public bool transmitting = false;
+    public Vector3 transPosition;
+    public string transType = "Default";
+    public bool withinView = false;
+    public bool transmitting = false;
 
-	public RangeDetector rangeDetector;
-	public MicAnalyzer micAnalyzer;
+    public RangeDetector rangeDetector;
+    public MicAnalyzer micAnalyzer;
 
-	void Start()
-	{
-		transPosition = transform.position;
-		rangeDetector = GameObject.Find ("RangeDetector").GetComponent<RangeDetector> ();
-		micAnalyzer = GameObject.Find ("MicController").GetComponent<MicAnalyzer> ();
-	}
+    void Start()
+    {
+        transPosition = transform.position;
+        rangeDetector = GameObject.Find ("RangeDetector").GetComponent<RangeDetector> ();
+        micAnalyzer = GameObject.Find ("MicController").GetComponent<MicAnalyzer> ();
+    }
 
-	void FixedUpdate () {
-		if (withinView) {
-			// Spin antenna
-			foreach (Transform child in transform) {
-				if (child.name.Contains ("transmitter.antenna")) {
-					child.Rotate (Vector3.up * Time.fixedDeltaTime * 200f, Space.World);
-				}
-			}
+    void FixedUpdate () {
+        Locomotion loc = transform.parent.GetComponent<Locomotion> ();
 
-			// Play the note
-			if (!transform.Find ("EmitSound").GetComponent<AudioSource> ().isPlaying) {
-				transform.Find ("EmitSound").GetComponent<AudioSource> ().Play ();
-			}
+        if (withinView) {
+            // Spin antenna
+            foreach (Transform child in transform) {
+                if (child.name.Contains ("transmitter.antenna")) {
+                    child.Rotate (Vector3.up * Time.fixedDeltaTime * 200f, Space.World);
+                }
+            }
 
-			// Look for matching pitch
-			if (micAnalyzer.curDb < micAnalyzer.DbThresh) {
-				transmitting = false;
-				transform.parent.GetComponent<Locomotion> ().Stop ();
-				return;
-			}
+            // Play the note
+            if (!transform.Find ("EmitSound").GetComponent<AudioSource> ().isPlaying) {
+                transform.Find ("EmitSound").GetComponent<AudioSource> ().Play ();
+            }
 
-			float pitch = micAnalyzer.curPitch;
-			TransmitterInfo matchingTransmitter = rangeDetector.getTransmitterFromPitch (pitch);
-			if (matchingTransmitter.name == transType) {
-				//Debug.Log (string.Format ("Transmitter {0} - Within View: {1} ; Transmitting: {2}", transType, withinView.ToString (), transmitting.ToString ()));
-				transmitting = true;
 
-				// JASPER:  Here is where we do the thing
-				transform.parent.GetComponent<Locomotion> ().Move ();
-			} else {
-				transmitting = false;
-				transform.parent.GetComponent<Locomotion> ().Stop ();
-			}
-		} else {
-			transmitting = false;
-			transform.parent.GetComponent<Locomotion> ().Stop ();
-		}
-	}
+            // Look for matching pitch
+            if (micAnalyzer.curDb < micAnalyzer.DbThresh) {
+                transmitting = false;
+                if (loc) {
+                    loc.Stop ();
+                }
+                return;
+            }
 
-	public Vector3 getTransPosition()
-	{
-		return transPosition;
-	}
+            float pitch = micAnalyzer.curPitch;
+            TransmitterInfo matchingTransmitter = rangeDetector.getTransmitterFromPitch (pitch);
+            if (matchingTransmitter.name == transType) {
+                //Debug.Log (string.Format ("Transmitter {0} - Within View: {1} ; Transmitting: {2}", transType, withinView.ToString (), transmitting.ToString ()));
+                transmitting = true;
 
-	public void resetState() {
-		withinView = false;
-		transmitting = false;
-	}
+                // JASPER:  Here is where we do the thing
+                if (loc) {
+                    loc.Move ();
+                } else if (transform.parent.GetComponent<Forklift> ()) {
+                    transform.parent.GetComponent<Forklift> ().broILift ();
+                }
+            } else {
+                transmitting = false;
+                if (loc) {
+                    loc.Stop ();
+                }
+            }
+        } else {
+            transmitting = false;
+            if (loc) {
+                loc.Stop ();
+            }
+        }
+    }
 
-	void OnTriggerEnter(Collider col) {
-		if (col.gameObject.tag == "GazeDetector") {
-			withinView = true;
-		}
-	}
+    public Vector3 getTransPosition()
+    {
+        return transPosition;
+    }
 
-	void OnTriggerExit(Collider col) {
-		if (col.gameObject.tag == "GazeDetector") {
-			withinView = false;
-		}
-	}
+    public void resetState() {
+        withinView = false;
+        transmitting = false;
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "GazeDetector") {
+            withinView = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.tag == "GazeDetector") {
+            withinView = false;
+        }
+    }
 }
